2247622cc27532a317686500fd8df2f38bb73880
Kooker The Fox! <62410083+KookerFoxYT@users.noreply.github.com>
2023-04-26 20:24:55 -0400

    Update MasterEditorMenu.hx

--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -18,6 +18,7 @@ class MasterEditorMenu extends MusicBeatState
 		'Menu Character Editor',
 		'Dialogue Editor',
 		'Dialogue Portrait Editor',
+		'Credits Editor',
 		'Character Editor',
 		'Chart Editor'
 	];
@@ -117,6 +118,8 @@ class MasterEditorMenu extends MusicBeatState
 					LoadingState.loadAndSwitchState(new DialogueCharacterEditorState(), false);
 				case 'Dialogue Editor':
 					LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
+				case 'Credits Editor':
+					MusicBeatState.switchState(new CreditsEditor());
 				case 'Chart Editor'://felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
 			}
